ec760306b10be1129777111be5e972fe69c1c974
David Minier <david.minier@groupama.com>
2024-12-24 15:47:28 +0100

    clean

--- myproject/application/security/token.py
+++ myproject/application/security/token.py
@@ -5,7 +5,7 @@ from typing import Any, Dict, List, Optional
 @dataclass
 class JWTAccessToken:
     iss: str
-    exp: int  # Expiration time (Unix timestamp)
+    exp: int
     aud: str
     sub: str
     iat: int
@@ -14,9 +14,7 @@ class JWTAccessToken:
     # Optional fields for authentication information
     auth_time: Optional[str] = None
     acr: Optional[str] = None
-    amr: Optional[List[str]] = field(
-        default_factory=list
-    )  # Authentication Methods References
+    amr: Optional[List[str]] = field(default_factory=list)
 
     extra_claims: Dict[str, Any] = field(
         default_factory=dict
@@ -36,15 +34,12 @@ def map_payload_to_jwt_access_token(payload: dict) -> JWTAccessToken:
         "amr",
     }
 
-    # Extraire les champs standard du payload
     standard_data = {field: payload.get(field) for field in standard_fields}
 
-    # Capturer les champs supplémentaires non spécifiés dans la dataclass
     extra_claims = {
         key: value for key, value in payload.items() if key not in standard_fields
     }
 
-    # Construire l'objet JWTAccessToken
     return JWTAccessToken(
         **{key: value for key, value in standard_data.items() if value is not None},
         extra_claims=extra_claims,
